0aa4571b7f0a5f5258e15cafe24152be42ad85e7
m0ses-furaha <moses.furaha@amalitech.com>
2025-07-14 10:01:49 +0200

    feat(add frontend angular setup add Tailwind for styling add daisy ui for component styling

--- apps/pet-markt-be/src/app/products/products.service.ts
+++ apps/pet-markt-be/src/app/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateProductInput } from './dto/create-product.input';
 import { UpdateProductInput } from './dto/update-product.input';
 import { PrismaService } from '../prisma/prisima.service';
+import { Product } from '@prisma/client';
 
 @Injectable()
 export class ProductsService {
@@ -19,9 +20,22 @@ export class ProductsService {
 }
 
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
+  findOne(id: string) {
+return this.prisma.product.findFirst({
+  where: { id}
+})  }
+
+searchProducts(searchTerm: string): Promise<Product[]> {
+  const lowerCaseSearchTerm = searchTerm.toLowerCase();
+  return this.prisma.product.findMany({
+    where:{
+      OR:[
+        { name: { contains: lowerCaseSearchTerm, mode: 'insensitive' } },
+        { description: { contains: lowerCaseSearchTerm, mode: 'insensitive' } }
+      ]
+    }
+  })
+}
 
   update(id: number, updateProductInput: UpdateProductInput) {
     return `This action updates a #${id} product`;
